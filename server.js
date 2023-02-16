3f1b26136dbf601efed8faa4affc49b565639382
Ayush Singh <ayushsops@gmail.com>
2023-02-16 16:43:42 +0530

    making use of the config.env

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
 const app = require("./app");
 
+
 // START THE SERVER
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is up on port ${port}📡`);
 });
