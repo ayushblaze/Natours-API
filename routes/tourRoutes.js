6ea3cb58a16652dd42bb6421a28bb472f7f2b95f
Ayush Singh <ayushsops@gmail.com>
2023-02-20 16:37:38 +0530

    Adding an aggregation pipeline, along with it's route

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,11 +1,15 @@
-const express = require("express");
-const tourController = require("./../controllers/tourController");
+const express = require('express');
+const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
 // router.param("id", tourController.checkID);
 
-router.route("/top-5-cheap").get(tourController.aliasTopTours, tourController.getAllTours);
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+
+router.route("/tour-stats").get(tourController.getTourStats);
 
 router
   .route('/')
