80e8e981c53db16457e42f3ede088a33248290b3
Ayush Singh <ayushsops@gmail.com>
2023-02-20 17:52:20 +0530

    Adding multiple stages to the aggregation pipeline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -161,7 +161,7 @@ exports.getTourStats = async (req, res) => {
       },
       {
         $group: {
-          _id: "$difficulty",
+          _id: { $toUpper: "$difficulty"},
           num: { $sum: 1 },
           numRatings: { $sum: "$ratingsQuantity" },
           avgRating: { $avg: "$ratingsAverage" },
@@ -169,7 +169,13 @@ exports.getTourStats = async (req, res) => {
           minPrice: { $min: "$price" },
           maxPrice: { $max: "$price" },
         }
-      }
+      },
+      {
+        $sort: { avgPrice: 1 }
+      },
+      // {
+      //   $match: { _id: { $ne: "EASY" } }  
+      // }
     ]);
 
     res.status(200).json({
@@ -177,6 +183,59 @@ exports.getTourStats = async (req, res) => {
       data: {
         stats,
       },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: "$startDates" 
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          }
+        }
+      },
+      {
+        $group: {
+          _id: { $month: "$startDates" },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: "$name" },
+        }
+      },
+      {
+        $addFields: { month: "$_id" } 
+      },
+      {
+        $project: {
+          _id: 0
+        }
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
+      {
+        $limit: 12
+      }
+    ]);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        plan
+      }
     });
   } catch (err) {
     res.status(404).json({
