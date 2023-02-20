e81a4f91cf8c8ee6ec2fe5e911427097519b56f8
Ayush Singh <ayushsops@gmail.com>
2023-02-20 08:24:42 +0530

    Implementing sorting using multiple fields

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -18,7 +18,10 @@ exports.getAllTours = async (req, res) => {
 
     // 2) SORTING
     if (req.query.sort) {
-      query = query.sort(req.query.sort); 
+      const sortBy = req.query.sort.split(",").join(" ");
+      query = query.sort(sortBy); 
+    } else {
+      query = query.sort("-createdAt");
     }
 
     // EXECUTE QUERY
