8ee77368970b52c7aa223e149bfffde568ca1e30
Ayush Singh <ayushsops@gmail.com>
2023-02-21 09:00:34 +0530

    Query (db query) middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -53,7 +53,11 @@ const tourSchema = new mongoose.Schema({
     default: Date.now(),
     select: false,
   },
-  startDates: [Date]
+  startDates: [Date],
+  secretTour: {
+    type: Boolean,
+    default: false,
+  }
 }, {
   toJSON: { virtuals: true },
   toObject: { virtuals: true },
@@ -75,6 +79,22 @@ tourSchema.pre("save", function(next) {
 // they also have access to the doc that was just saved
 tourSchema.post("save", function(doc, next) {
   console.log(doc);
+  next();
+});
+
+// QUERY MIDDLEWARE 
+// This middleware would run before running any find query.
+// The "this" keyword would now point at the current query and not the current doc
+// tourSchema.pre("find", function(next) {
+  tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } });
+  this.start = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} ms`);
+  // console.log(docs);
   next();
 });
 
