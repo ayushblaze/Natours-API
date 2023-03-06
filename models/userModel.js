3e81117f188c22287066b785c278a671dbf9e8b5
Ayush Singh <ayushsops@gmail.com>
2023-03-06 08:56:43 +0530

    Hasing passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -29,9 +30,17 @@ const userSchema = new mongoose.Schema({
         return el === this.password;
       }
     },
-    message: "Password don't match",
-    select: false, // makes the password not show up in any output 
+    message: "Passwords don't match",
   }
+});
+
+userSchema.pre("save", async function(next){
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
